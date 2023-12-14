06ded2f45023a42651e8885829afde30277b7136
Bas Magré <Opvolger@gmail.com>
2023-12-14 23:35:17 +0100

    eerste opzet

---
+++ extract.sh
@@ -0,0 +1,23 @@
+version="21.2"
+url=https://mirrors.layeronline.com/linuxmint/stable/$version/linuxmint-$version-cinnamon-64bit.iso
+filename=$(basename $url)
+basedir=$(pwd)
+
+echo "url: $url"
+echo "filename: $filename"
+
+if [[ -f "$filename" ]]; then
+    echo "$filename exists."
+else
+    echo "download $filename."
+    wget $url
+fi
+
+
+mkdir -p $basedir/pxe/linuxmint
+mkdir -p $basedir/pxe/ipxe
+#rm $basedir/pxe/linuxmint -rf
+#7z x $filename -o$basedir/pxe/linuxmint
+chmod -R u=rwx,go=rx $basedir/pxe/linuxmint
+cp $filename $basedir/pxe/$filename
+sed -i "s/linuxmintversion \"[0-9.]*/linuxmintversion \"$version/g" $(pwd)/pxe/ipxe/boot.ipxe
